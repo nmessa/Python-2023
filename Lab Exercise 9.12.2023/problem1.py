934dd19010be4b3bf9dc1e839d8a47731f221dd0
nmessa <nmessa@sau16.org>
2023-09-08 11:31:21 -0400

    Add files via upload

---
+++ Lab Exercise 9.12.2023/problem1.py
@@ -0,0 +1,9 @@
+## Lab Exercise 9.12.2023 Problem 1
+## Author: 
+## prints the even numbers <= 1000
+
+#Add code here
+
+
+    
+
---
+++ Lab Exercise 9.12.2023/problem4.py
@@ -0,0 +1,11 @@
+## Lab Exercise 9.12.2023 Problem 4
+## Author: 
+## rocket countdown
+import time
+
+#Add code here
+
+
+   
+    
+
