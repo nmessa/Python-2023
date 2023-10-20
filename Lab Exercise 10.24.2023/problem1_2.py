36536c1081f476fb2f240a0fa4a31d6a6a295f54
nmessa <nmessa@sau16.org>
2023-10-20 07:20:06 -0400

    Add files via upload

---
+++ Lab Exercise 10.24.2023/problem1_2.py
@@ -0,0 +1,21 @@
+## Lab Exercise 10/24/2023 Problem 1 and 2
+## Author: 
+## Create a bank account class and test it
+from bankAccount import BankAccount
+from interestAccount import InterestAccount
+
+
+#Create test objects
+#Add code here
+
+
+
+#Test class methods
+#Add code here
+
+
+
+#print the accounts
+#Add code here
+
+
