d7454bb390005cb9c90cb7ba19b297da5df59d6d
nmessa <nmessa@sau16.org>
2023-10-27 14:01:24 -0400

    Add files via upload

---
+++ Lab Exercise 11.1.2023/dna2.py
@@ -0,0 +1,18 @@
+## Lab Exercise 11/1/2023 Problem 2
+## Author: 
+## This program demonstrates DNA sequence matching
+
+import random
+BASES = ['G', 'A', 'T', 'C']
+strand = ""
+
+#generate a DNA string of one million bases
+    # ADD CODE HERE
+        
+
+#Enter DNA sequence to search for
+    # ADD CODE HERE
+
+#search sequence
+    # ADD CODE HERE
+
