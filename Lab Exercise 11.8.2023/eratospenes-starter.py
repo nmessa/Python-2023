1496b505ce8e9031547288d19be26a39a36f4db9
nmessa <nmessa@sau16.org>
2023-11-03 10:10:04 -0400

    Add files via upload

---
+++ Lab Exercise 11.8.2023/eratospenes-starter.py
@@ -0,0 +1,25 @@
+#Lab Exercise 11/8/2023 Problem 1
+#eratosthenes-starter.py
+#Author: 
+#Generates a list of prime numbers using Eratsothenes algorithm
+
+#ADD CODE HERE
+
+
+#initialize the list
+#ADD CODE HERE
+
+
+
+#execute the sieve
+#ADD CODE HERE
+
+
+
+
+#build the list of primes
+#ADD CODE HERE
+
+
+
+print (primeList)
---
+++ Lab Exercise 11.9.2023/findPi.py
@@ -0,0 +1,16 @@
+## Lab Exercise 11/9/2023 Problem 7
+## Author: 
+## Program to determine the value of Pi
+
+import random
+
+SIZE = 1000000
+count = 0
+
+#Add code here
+
+
+print ("With", SIZE, "trials Pi =", pi)
+
+##Output
+##With 1000000 trials Pi = 3.141164
