144313a5fcfab086359a191ec78e60fac05bc0bb
nmessa <nmessa@sau16.org>
2023-11-18 10:39:54 -0500

    Add files via upload

---
+++ Lab Exercise 11.20.2023/problem2.py
@@ -0,0 +1,14 @@
+## Lab Exercise 11/20/2023 Problem 2
+## Author: 
+## Function that returns a boolean value if word is a palindrome
+
+
+
+def isPalindrome(word):
+    #Add code here
+    
+    
+print (isPalindrome('KAYAK'))     #True
+print (isPalindrome('Hello'))     #False
+print (isPalindrome('abccba'))    #True
+        
