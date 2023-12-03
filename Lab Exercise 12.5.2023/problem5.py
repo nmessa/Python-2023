86912b3583c6d27016f34cae8b0598c8ed736ce1
nmessa <nmessa@sau16.org>
2023-12-03 09:33:28 -0500

    Add files via upload

---
+++ Lab Exercise 12.5.2023/problem5.py
@@ -0,0 +1,13 @@
+##Lab Exercise 12.5.2023 Problem 5
+##Author: 
+##Remove all characters with an odd index
+
+sample = "restart"
+newString = ""
+#Add code here
+
+
+print(newString)
+    
+##Output
+##rsat
