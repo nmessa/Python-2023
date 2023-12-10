24435837956012536f1284d96e78c2e24b59a9f8
nmessa <nmessa@sau16.org>
2023-12-10 10:55:53 -0500

    Add files via upload

---
+++ Lab Exercise 12.14.2023/problem4.py
@@ -0,0 +1,14 @@
+## Lab Exercise 12/14/2023 Problem 4
+## Author: 
+def molecularWeight(formula):
+    #Add code here
+
+    
+    
+#Test code
+print (molecularWeight('C6H12O6'))
+print (molecularWeight('C16H2O12'))
+
+## Output
+## 180.0972
+## 386.02459999999996
