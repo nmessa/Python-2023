beeabdd670ff63e9cea8f7021a6e82df94ffd121
nmessa <nmessa@sau16.org>
2023-12-22 11:14:06 -0500

    Add files via upload

---
+++ Lab Exercise 1.5.2024/problem3.py
@@ -0,0 +1,15 @@
+##Lab Exercise 1.5.2024 Problem 3
+##Author: 
+
+def xmasItems(n):
+    #Add code here
+    
+
+print(xmasItems(12))
+print(xmasItems(3))
+print(xmasItems(31))
+
+##Output
+##364
+##10
+##5456
